101413de35fdbc3900061872c252ae61c6cb2f4b
Uladzimir Tsykun <tsykun314@gmail.com>
2023-09-02 23:21:42 +0200

    Mass import packages from Packagist.com/Satis or VCS

--- public/packeton/js/import.js
+++ public/packeton/js/import.js
@@ -1,7 +1,17 @@
 (function ($) {
     let form = $('#submit-package-form');
     let onSubmit = function(e) {
+        let handler = (data) => {
 
+        };
+
+        $('#submit').val('Submit');
+        form.unbind('submit');
+
+        let url = $(this).data('check-url');
+        $.post(url, $(this).serializeArray(), handler);
+        $('#submit').addClass('loading');
+        e.preventDefault();
     };
 
     $('.type-hide').closest('.form-group').hide();
@@ -16,5 +26,36 @@
         $('.'+repoType).closest('.form-group').show();
     });
 
+    let select = $('.integration-select');
+
+    select.on('change', (e) => {
+        let val = select.val();
+        let s2 = $('.integration-repo');
+        if (!val) {
+            s2.select2({'data': []});
+            s2.html('').change();
+            return;
+        }
+
+        let url = '/integration/all/' + val + '/repos';
+        $.ajax({
+            url: url,
+            success: (result) => {
+                let options = result.map((item) => '<option value="' + item.id + '">' + item.text + '</option>');
+                s2.select2({'data': result});
+                s2.html(options.join('')).change();
+            }
+        });
+    });
+
     typeSelect.triggerHandler('change');
+
+    let packRepo = $('.package-repo-info');
+    packRepo.change(function() {
+        form.unbind('submit');
+        form.submit(onSubmit);
+        $('#submit').val('Check');
+    });
+    packRepo.triggerHandler('change');
+
 })(jQuery);
--- src/Form/Type/Package/ImportPackagesType.php
+++ src/Form/Type/Package/ImportPackagesType.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Packeton\Form\Type\Package;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Packeton\Entity\OAuthIntegration;
 use Packeton\Form\Type\CredentialType;
+use Packeton\Integrations\IntegrationRegistry;
+use Packeton\Util\PacketonUtils;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -13,9 +16,17 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ImportPackagesType extends AbstractType
 {
+    public function __construct(
+        protected ManagerRegistry $registry,
+        protected IntegrationRegistry $integrations,
+    ) {
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -28,7 +39,7 @@ class ImportPackagesType extends AbstractType
                     'VCS repos' => 'vcs',
                     'Integration GitHub/GitLab/Gitea/Bitbucket' => 'integration',
                 ],
-                'attr' => ['class' => 'package-repo-type']
+                'attr' => ['class' => 'package-repo-type package-repo-info']
             ])
             ->add('clone', ChoiceType::class, [
                 'label' => 'Clone preference',
@@ -52,20 +63,63 @@ class ImportPackagesType extends AbstractType
             ->add('integration', EntityType::class, [
                 'class' => OAuthIntegration::class,
                 'required' => false,
-                'attr' => ['class' => 'type-hide integration']
+                'attr' => ['class' => 'type-hide integration package-repo-info integration-select']
+            ])
+            ->add('integrationRepos', ChoiceType::class, [
+                'label' => 'Import only these repos (default all)',
+                'multiple' => true,
+                'required' => false,
+                'attr' => ['class' => 'jselect2 type-hide integration integration-repo package-repo-info']
             ])
             ->add('username', TextType::class, [
                 'required' => false,
-                'attr' => ['class' => 'type-hide composer']
+                'attr' => ['class' => 'type-hide composer package-repo-info']
             ])
             ->add('password', TextType::class, [
                 'required' => false,
-                'attr' => ['class' => 'type-hide composer']
+                'attr' => ['class' => 'type-hide composer package-repo-info']
             ])
             ->add('list', TextareaType::class, [
                 'label' => 'List of VCS repos',
                 'required' => false,
                 'attr' => ['class' => 'type-hide vcs', 'placeholder' => "git@github.com:thephpleague/flysystem.git\ngit@github.com:vtsykun/packeton.git", 'rows' => 8],
             ]);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, $this->updateIntegration(...));
+        $builder->addEventListener(FormEvents::POST_SET_DATA, $this->updateIntegration(...));
+    }
+
+    public function updateIntegration(FormEvent $event): void
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (!$integration = $data['integration'] ?? null) {
+            return;
+        }
+        if (is_numeric($integration)) {
+            $integration = $this->registry->getRepository(OAuthIntegration::class)->find((int) $integration);
+            if (!$integration instanceof OAuthIntegration) {
+                return;
+            }
+        }
+
+        $choices = $this->getReposChoices($integration);
+        $form->add('integrationRepos', ChoiceType::class, [
+            'label' => 'Repository name',
+            'multiple' => true,
+            'required' => false,
+            'choices' => $choices,
+            'attr' => ['class' => 'jselect2 type-hide integration package-repo-info']
+        ]);
+    }
+
+    protected function getReposChoices(OAuthIntegration $oauth): array
+    {
+        $app = $this->integrations->findApp($oauth->getAlias(), false);
+        $repos = $app->repositories($oauth);
+
+        $repos = $oauth->filteredRepos($repos, true);
+        return PacketonUtils::buildChoices($repos, 'text', 'id');
     }
 }
